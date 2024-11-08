2256261ef84def0f21bbee577ff8a017b53f67a5
Jay Jie <jyz1992@hotmail.com>
2024-11-07 16:11:46 -0800

    chore: add type Txs

--- types/types.go
+++ types/types.go
@@ -7,5 +7,8 @@ import (
 // Tx represents a transaction in the form of a byte slice.
 type Tx []byte
 
+// Txs represents a slice of transactions.
+type Txs []Tx
+
 // Hash is a type alias for header.Hash
 type Hash = header.Hash
