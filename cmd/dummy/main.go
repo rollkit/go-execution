09d6483c93f69d726e46eb0a1c42430271633413
Tomasz Zdybał <tomek@zdybal.lap.pl>
2024-12-03 17:16:09 +0100

    refactor: improve flag parsing and logs

--- cmd/dummy/main.go
+++ cmd/dummy/main.go
@@ -2,37 +2,36 @@ package main
 
 import (
 	"errors"
-	"google.golang.org/grpc"
+	"flag"
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"google.golang.org/grpc"
+
 	grpcproxy "github.com/rollkit/go-execution/proxy/grpc"
 	"github.com/rollkit/go-execution/test"
 	pb "github.com/rollkit/go-execution/types/pb/execution"
 )
 
-const bufSize = 1024 * 1024
-
 func main() {
-	dummy := test.NewDummyExecutor()
-
-	listenAddress := "127.0.0.1:40041"
-	if len(os.Args) == 2 {
-		listenAddress = os.Args[1]
+	listenAddress, err := parseListenAddress()
+	if err != nil {
+		log.Fatalf("Failed to parse listen address: %v\n", err)
 	}
-
 	listener, err := net.Listen("tcp4", listenAddress)
 	if err != nil {
-		log.Fatalf("error while creating listener: %v\n", err)
+		log.Fatalf("Failed to listen on %q: %v\n", listenAddress, err)
 	}
 	defer func() {
 		_ = listener.Close()
 	}()
 
-	log.Println("Starting server...")
+	log.Println("Creating Dummy Executor and gRPC server")
+	dummy := test.NewDummyExecutor()
 	server := grpcproxy.NewServer(dummy, grpcproxy.DefaultConfig())
 	s := grpc.NewServer()
 	pb.RegisterExecutionServiceServer(s, server)
@@ -43,7 +42,7 @@ func main() {
 
 	doneChan := make(chan interface{}, 1)
 	go func() {
-		log.Println("Serving...")
+		log.Printf("Serving (%s)...\n", listenAddress)
 		log.Println("Type Ctrl+C to shutdown")
 		if err := s.Serve(listener); err != nil && !errors.Is(err, grpc.ErrServerStopped) {
 			log.Fatalf("Server exited with error: %v\n", err)
@@ -60,4 +59,20 @@ func main() {
 
 	<-doneChan
 	log.Println("Server stopped")
+}
+
+func parseListenAddress() (string, error) {
+	var listenAddress string
+	flag.StringVar(&listenAddress, "address", "127.0.0.1:40041", "gRPC server listen address")
+	flag.Parse()
+
+	_, port, err := net.SplitHostPort(listenAddress)
+	if err != nil {
+		return "", fmt.Errorf("invalid address format %q: %v", listenAddress, err)
+	}
+	if port == "" {
+		return "", errors.New("port cannot be empty")
+	}
+
+	return listenAddress, nil
 }
